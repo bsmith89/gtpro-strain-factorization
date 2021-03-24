0c0a75dcca72742728b9d8592fa3c711dbd2201b
Byron Smith <me@byronjsmith.com>
2021-03-24 14:49:13 -0700

    Update notebook output filter to drop id tags.

--- scripts/ipynb_output_filter.py
+++ scripts/ipynb_output_filter.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+jq 'del(.cells[] ["id", "outputs", "execution_count", "output_type"])'
+
+"""
 
 import json
 import sys
@@ -12,6 +16,8 @@ if __name__ == "__main__":
             cell["execution_count"] = None
         if "output_type" in cell:
             del cell["output_type"]
+        if "id" in cell:
+            del cell["id"]
         cell["metadata"] = {}
 
     json.dump(nb, sys.stdout, sort_keys=True, indent=1)
